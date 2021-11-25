e3d1727b9f151c2f13fa74933ec3dfe6f953b0e7
Sowmyashree K <90288071+sowmyashreek@users.noreply.github.com>
2021-11-25 11:09:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY>50){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+
+    })
+});
